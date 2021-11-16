71ac22b8f445cb0ccff0557e3a803222be4b7d57
Brian Gilmour <b.c.gilmour@medisin.uio.no>
2021-11-16 14:52:45 +0100

    Added comments

--- seq_lengths.py
+++ seq_lengths.py
@@ -3,10 +3,14 @@ import sys
 with open(sys.argv[1], "r") as fh:
     lines = fh.readlines()
 
+
 for line in lines:
+    # remove new lines
     line = line.strip()
     if line.startswith(">"):
+        # record header
         header = line
     else:
+        # record length of non-header
         length = len(line)
         print(length, header)
